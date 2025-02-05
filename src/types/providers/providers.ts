6eabe615ef2608f8af49d5dce62171679973721f
Santiago Florez <santiagoflorezv23@gmail.com>
2025-02-05 15:06:34 -0500

    feat: :sparkles: update provider API endpoint and enhance provider model with bank account details; add pagination component

--- src/types/providers/providers.ts
+++ src/types/providers/providers.ts
@@ -1,12 +1,17 @@
+export interface BankAccount {
+  id?: number;
+  bank_account: string;
+  type_account: string;
+  bank: string;
+}
+
 export interface Provider {
-    id: number;
-    name: string;
-    nit: string;
-    email: string;
-    phone: string;
-    address: string;
-    bank_account: string;
-    type_account: string;
-    bank: string;
-    status: boolean;
-  }
+  id: number;
+  name: string;
+  nit: string;
+  email: string;
+  phone: string;
+  address: string;
+  bankAccounts: BankAccount[];
+  status: boolean;
+}
