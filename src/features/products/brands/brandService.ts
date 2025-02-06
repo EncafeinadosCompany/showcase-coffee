a9e264db1e0f5cd468d8c5e833cd16d33fdd2c0f
Dm29015 <dmcorrea670@soy.sena.edu.co>
2025-02-06 17:09:58 -0500

    refactor: :recycle: routes

--- src/features/products/brands/brandService.ts
+++ src/features/products/brands/brandService.ts
@@ -3,29 +3,29 @@ import { brandType } from "@/types/products/brand";
 
 
 export const getBrandById = async (id: string | number) => {
-  const response = await axiosInstance.get(`/brands/${id}`);
+  const response = await axiosInstance.get(`/products/brands/${id}`);
   return response.data;
 };
 
 
 export const getBrands = async () => {
-  const response = await axiosInstance.get("/brands");
+  const response = await axiosInstance.get("/products/brands");
   return response.data;
 };
 
 
 export const createBrand = async (brand: Omit<brandType, "id">) => {
-  const response = await axiosInstance.post("/brands", brand);
+  const response = await axiosInstance.post("/products/brands", brand);
   return response.data;
 };
 
 
 export const updateBrand = async (id: string | number, brand: Partial<brandType>) => {
-  const response = await axiosInstance.put(`/brands/${id}`, brand);
+  const response = await axiosInstance.put(`/products/brands/${id}`, brand);
   return response.data;
 };
 
 
 export const deleteBrand = async (id: string | number) => {
-  await axiosInstance.delete(`/brands/${id}`);
+  await axiosInstance.delete(`/products/brands/${id}`);
 };
