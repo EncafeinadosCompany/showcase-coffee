99f3c4a80e04d54acccff50d4bb03fdd41096f4a
Dm29015 <dmcorrea670@soy.sena.edu.co>
2025-02-21 13:17:41 -0500

    style: :art: standardization of the design of the tables

--- src/layout/content/content.tsx
+++ src/layout/content/content.tsx
@@ -3,13 +3,13 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div className="flex h-screen w-full bg-[#F5E6D3] overflow-hidden">
+    <div className="flex h-screen w-full bg-[#FAF3E7] overflow-hidden">
       <div className="hidden m-4 md:flex md:flex-shrink-0">
         <Sidebar />
       </div>
       <div className="flex-1 flex flex-col w-full overflow-hidden">
-        <main className="flex-1 p-4 overflow-hidden">
-          <div className="h-full w-full mx-auto max-w-8xl rounded-2xl overflow-auto">
+        <main className="flex-1 m-4 overflow-hidden">
+          <div className="h-full  w-full  rounded-2xl overflow-auto ">
             <Outlet />
           </div>
         </main>
